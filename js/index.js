b56fefe72ba2ed443c8b85f7998a76467189eb3f
Shaiyrgul <shaiyrgul.manapova.kk@gmail.com>
2022-02-09 02:51:37 +0600

    added background and JS

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// Game Constans & Variables
+let direction = {x: 0, y: 0};
+let foodSound = new Audio("music/food.mp3");
+let gameOverSound = new Audio("music/gameover.mp3");
+let moveSound = new Audio("music/move.mp3");
+let musicSound = new Audio("music/music.mp3");
+
+// Game Functions
+function main(ctime) {
+	window.requestAnimationFrame(main);
+	if ((ctime - lastPaintTime)/1000 < 1/speed);
+
+}
+
+
+// Main logic starts here
